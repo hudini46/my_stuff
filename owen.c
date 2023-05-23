c5fa90d8cc6583f7e43e2c5d7f39ea1a75af3868
hudini46 <owenkiilu37@gmail.com>
2023-05-23 13:54:07 -0700

    first program

---
+++ owen.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main ()
+{
+	int a;
+	int b;
+	char c;
+	float d;
+	
+	a=100;
+	b=10000;
+	c='j';
+	d=2.525;
+	printf ("if i make %d\n and you make %c\ ohn will owe me %d\n%f\n",a,c,b,d);
+	
+
+    return 0;
+}    
+
+	
