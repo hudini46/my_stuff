28f80898068b9ba865263ad3684d8459fe7fd76e
hudini46 <owenkiilu37@gmail.com>
2023-05-26 02:44:17 -0700

    loopcalc

--- cal.c
+++ cal.c
@@ -2,8 +2,12 @@
 #include<stdlib.h>
 int main ()
 { 
+	int x;
 	float a,b,c;
-	char q;
+	char q,j;
+	x=2;
+	do
+	{
 	system("clear");
 	printf("ENTER ARITHMETICS!\n");
         scanf("%f%c%f",&a,&q,&b);
@@ -15,5 +19,11 @@ int main ()
 	case '/' : c=a/b; printf("=%f\n",c); break;	   
 	default : printf("cannot perfom function\n"); break;	   
 	}
-return 0;		  
+	printf("Press enter to continue...\n");
+	scanf("%c",&j);
+	getchar();
+	x++;
+        }while (x!=1);
+
+	return 0;		  
 }       
